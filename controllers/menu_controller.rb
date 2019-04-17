df22809942ae8a9ed214c39efc1b42f5d68a986f
Michael Davis <mdavis93@hotmail.com>
2019-04-17 02:28:43 -0500

    Checkpoint Complete

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -198,17 +198,20 @@ class MenuController
   end
 
   def edit_entry(entry)
+    updates = {}
     print 'Updated Name: '
     name = gets.chomp
+    updates[:name] = name unless name.empty?
     print 'Updated Phone Number: '
-    phone = gets.chomp
+    phone_number = gets.chomp
+    updates[:phone_number] = phone_number unless phone_number.empty?
     print 'Updated Email: '
     email = gets.chomp
-    entry.name = name if !name.empty?
-    entry.phone_number = phone if !phone.empty?
-    entry.email = email if !email.empty?
+    updates[:email] = email unless email.empty?
+    entry.update_attributes(updates)
     system 'clear'
-    puts entry
+    puts 'Updated Entry:'
+    puts Entry.find(entry.id)
   end
 
   def delete_entry(entry)
