15f0857e8de45c0d0a4bd7795086e1a90e43a3fc
Michael Davis <mdavis93@hotmail.com>
2019-04-20 01:07:45 -0500

    Association Asgmt Complete

--- address_book.rb
+++ address_book.rb
@@ -1,7 +1,7 @@
 require_relative 'controllers/menu_controller'
 require 'record_manager'
 
-RecordManager.connect_to('db/address_book.sqlite')
+RecordManager.connect_to('db/address_book.db', :sqlite)
 
 menu = MenuController.new
 system "clear"
